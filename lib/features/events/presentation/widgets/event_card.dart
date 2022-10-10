bde7d671b331e112b60f3d9034ddadbab6df6c3e
sara <sarah.ibrahim1855@gmail.com>
2022-10-10 03:36:56 +0200

    first_commit

--- lib/features/events/presentation/widgets/event_card.dart
+++ lib/features/events/presentation/widgets/event_card.dart
@@ -13,6 +13,7 @@ class EventCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      
       children: [
         Card(
           shape: RoundedRectangleBorder(
